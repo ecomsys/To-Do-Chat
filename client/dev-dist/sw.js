fab892910366f81683a50640fa4ad94018aa05b4
ecomsys <ecomsysru@gmail.com>
2026-06-11 22:52:35 +0300

    update

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-aeb6ecaf'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7naiitgf0pc"
+    "revision": "0.sj482hajgg4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
